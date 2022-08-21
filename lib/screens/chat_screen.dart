95c2fc60bd9148f368ff3696e61338708cf9d776
Ekojode <58553973+Ekojode@users.noreply.github.com>
2022-08-21 16:51:27 +0100

    Set up simple stream logic

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -7,24 +7,43 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, i) {
-            return Container(
-              padding: const EdgeInsets.all(8),
-              child: const Text("This works"),
-            );
-          }),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("chats/FoyOKzh3SXyWreStUCVm/messages")
+            .snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: Text("No data from cloud"),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (ctx, i) {
+                  return Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text("This works"),
+                  );
+                },
+              );
+            }
+          }
+        },
+      ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {
             FirebaseFirestore.instance
                 .collection("chats/FoyOKzh3SXyWreStUCVm/messages")
                 .snapshots()
                 .listen((event) {
-              print("${event.docs[0]["text"]}  omo ope");
+              debugPrint("${event.docs}");
             });
           },
           child: const Icon(Icons.add)),
     );
   }
 }
+//"chats/FoyOKzh3SXyWreStUCVm/messages"
