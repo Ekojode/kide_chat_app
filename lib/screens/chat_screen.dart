d96f26e2677c72ca8ec47678e90433760458c87b
Ekojode <58553973+Ekojode@users.noreply.github.com>
2022-08-22 12:11:19 +0100

    Ordering and sending chat functionalities added

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,13 +1,15 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/chats/messages.dart';
+import '../widgets/chats/new_message.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    const collectionPath = "chats/FoyOKzh3SXyWreStUCVm/messages";
+    //  const collectionPath = "chats/FoyOKzh3SXyWreStUCVm/messages";
     final auth = FirebaseAuth.instance;
     return Scaffold(
       appBar: AppBar(
@@ -22,40 +24,12 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: StreamBuilder(
-        stream:
-            FirebaseFirestore.instance.collection(collectionPath).snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: Text("No data from cloud"),
-              );
-            } else {
-              final documents = snapshot.data!.docs;
-              return ListView.builder(
-                itemCount: documents.length,
-                itemBuilder: (ctx, i) {
-                  return Container(
-                    padding: const EdgeInsets.all(8),
-                    child: Text(documents[i]["text"]),
-                  );
-                },
-              );
-            }
-          }
-        },
+      body: Column(
+        children: const [
+          Expanded(child: Messages()),
+          NewMessage(),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            FirebaseFirestore.instance
-                .collection(collectionPath)
-                .add({"text": "This was added by clicking the button"});
-          },
-          child: const Icon(Icons.add)),
     );
   }
 }
-//"chats/FoyOKzh3SXyWreStUCVm/messages"
---
+++ lib/widgets/chats/messages.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  const Messages({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("chat")
+          .orderBy("createdAt", descending: true)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: Text("There are no chats yet, Start chatting now!"),
+            );
+          } else {
+            final documents = snapshot.data!.docs;
+            return ListView.builder(
+              reverse: true,
+              itemCount: documents.length,
+              itemBuilder: (ctx, i) {
+                return Text(
+                  documents[i]["text"],
+                );
+              },
+            );
+          }
+        }
+      },
+    );
+  }
+}
